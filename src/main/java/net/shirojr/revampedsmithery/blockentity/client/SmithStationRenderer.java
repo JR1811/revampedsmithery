4969ac288a430caed4443fe471c672dfa2147f33
JR1811 <janreichelt77@gmail.com>
2025-10-28 16:48:28 +0100

    set up basic armor rendering models

--- src/main/java/net/shirojr/revampedsmithery/blockentity/client/SmithStationRenderer.java
+++ src/main/java/net/shirojr/revampedsmithery/blockentity/client/SmithStationRenderer.java
@@ -1,14 +1,18 @@
 package net.shirojr.revampedsmithery.blockentity.client;
 
+import com.google.common.collect.Maps;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.*;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.entity.model.ArmorStandArmorEntityModel;
+import net.minecraft.client.render.entity.model.EntityModelLayers;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.DyeableArmorItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.RotationAxis;
@@ -18,15 +22,26 @@ import net.shirojr.revampedsmithery.blockentity.SmithStationBlockEntity;
 import net.shirojr.revampedsmithery.init.RevampedSmitheryBlocks;
 import net.shirojr.revampedsmithery.init.RevampedSmitheryModelLayers;
 import net.shirojr.revampedsmithery.mixin.access.DebugRendererAccess;
+import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3f;
+
+import java.util.Map;
 
 public class SmithStationRenderer implements BlockEntityRenderer<SmithStationBlockEntity> {
     public static final Identifier TEXTURE = RevampedSmithery.getId("textures/entity/smith_station.png");
+    private static final Map<String, Identifier> ARMOR_TEXTURE_CACHE = Maps.newHashMap();
 
     private final SmithStationModel model;
+    // private final BipedEntityModel<LivingEntity> bipedModel;
+    private final ArmorStandArmorEntityModel innerModel;
+    private final ArmorStandArmorEntityModel outerModel;
 
     public SmithStationRenderer(BlockEntityRendererFactory.Context ctx) {
         this.model = new SmithStationModel(ctx.getLayerModelPart(RevampedSmitheryModelLayers.SMITHING_STATION));
+        //this.bipedModel = new BipedEntityModel<>(ctx.getLayerModelPart(EntityModelLayers.PLAYER));
+
+        innerModel = new ArmorStandArmorEntityModel(ctx.getLayerModelPart(EntityModelLayers.ARMOR_STAND_INNER_ARMOR));
+        outerModel = new ArmorStandArmorEntityModel(ctx.getLayerModelPart(EntityModelLayers.ARMOR_STAND_OUTER_ARMOR));
     }
 
     @SuppressWarnings("unused")
@@ -43,6 +58,7 @@ public class SmithStationRenderer implements BlockEntityRenderer<SmithStationBlo
         }
 
         BlockState state = entity.getCachedState();
+        ItemStack armorStack = entity.getData().getArmorStack();
         int blockLight = WorldRenderer.getLightmapCoordinates(client.world, entity.getPos().up(2));
 
         matrices.push();
@@ -54,6 +70,13 @@ public class SmithStationRenderer implements BlockEntityRenderer<SmithStationBlo
 
         VertexConsumer vertexConsumer = vertexConsumers.getBuffer(this.model.getLayer(TEXTURE));
         this.model.render(matrices, vertexConsumer, blockLight, overlay, 1f, 1f, 1f, 1f);
+
+        if (armorStack.getItem() instanceof ArmorItem armorItem) {
+            matrices.push();
+            matrices.translate(0, -2, 0);
+            renderArmor(matrices, vertexConsumers, blockLight, armorStack, armorItem, armorItem.getSlotType());
+            matrices.pop();
+        }
         matrices.pop();
     }
 
@@ -66,5 +89,56 @@ public class SmithStationRenderer implements BlockEntityRenderer<SmithStationBlo
             WorldRenderer.drawBox(matrices, vertexConsumers.getBuffer(RenderLayer.LINES), hitBox.getRotatedBox(facing),
                     color.x, color.y, color.z, 1f);
         });
+    }
+
+    private void renderArmor(MatrixStack matrices, VertexConsumerProvider vertexConsumers,
+                             int light, ItemStack stack, ArmorItem armorItem,
+                             EquipmentSlot slot) {
+        boolean useInnerLayer = slot == EquipmentSlot.LEGS;
+        ArmorStandArmorEntityModel armorModel = useInnerLayer ? innerModel : outerModel;
+
+        armorModel.setVisible(false);
+
+        switch (slot) {
+            case HEAD -> armorModel.head.visible = true;
+            case CHEST -> {
+                armorModel.body.visible = true;
+                armorModel.leftArm.visible = true;
+                armorModel.rightArm.visible = true;
+            }
+            case LEGS -> {
+                armorModel.body.visible = true;
+                armorModel.leftLeg.visible = true;
+                armorModel.rightLeg.visible = true;
+            }
+            case FEET -> {
+                armorModel.leftLeg.visible = true;
+                armorModel.rightLeg.visible = true;
+            }
+        }
+
+        if (armorItem instanceof DyeableArmorItem dyeableArmorItem) {
+            int i = dyeableArmorItem.getColor(stack);
+            float r = (i >> 16 & 0xFF) / 255.0F;
+            float g = (i >> 8 & 0xFF) / 255.0F;
+            float b = (i & 0xFF) / 255.0F;
+            this.renderArmorParts(matrices, vertexConsumers, light, armorItem, armorModel, useInnerLayer, r, g, b, null);
+            this.renderArmorParts(matrices, vertexConsumers, light, armorItem, armorModel, useInnerLayer, 1.0F, 1.0F, 1.0F, "overlay");
+        } else {
+            this.renderArmorParts(matrices, vertexConsumers, light, armorItem, armorModel, useInnerLayer, 1.0F, 1.0F, 1.0F, null);
+        }
+    }
+
+    private void renderArmorParts(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, ArmorItem item,
+                                  ArmorStandArmorEntityModel armorModel, boolean useInnerLayer,
+                                  float red, float green, float blue, @Nullable String overlay) {
+        VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(this.getArmorTexture(item, useInnerLayer, overlay)));
+        armorModel.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV, red, green, blue, 1.0F);
+    }
+
+
+    private Identifier getArmorTexture(ArmorItem item, boolean secondLayer, @Nullable String overlay) {
+        String string = "textures/models/armor/" + item.getMaterial().getName() + "_layer_" + (secondLayer ? 2 : 1) + (overlay == null ? "" : "_" + overlay) + ".png";
+        return ARMOR_TEXTURE_CACHE.computeIfAbsent(string, Identifier::new);
     }
 }
